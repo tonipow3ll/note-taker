01a79e0ee7e1b0b72167c4c5518feabf65396610
Toni Powell <tonipow3ll@gmail.com>
2021-02-28 11:08:48 -0500

    moved things around, servers working

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
+// const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path');
+const fs = require('fs')
+// const htmlRoutes = require('routes/htmlroute');
+// const apiRoutes = require('routes/apiRoute');
 
 const app = express();
-
-// Sets an initial port. We"ll use this later in our listener
+// const bodyParser = require('body-parser')
+// // Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 3000;
-// const routes = require('somefile.js')
+// // const routes = require('somefile.js')
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+})
+
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
 
 // this line causes nodemon / server to crash
 // require('routes.js')(app);
 
-
-// LISTENER
-// The below code effectively "starts" our server
-
+// port listener
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
 });
-console.log("testing testing")
+
